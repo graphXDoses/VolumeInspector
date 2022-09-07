75cfc69a703e380f8d56bd69f58d799f16ed4685
Admin <Admin@DESKTOP-05LT3I1>
2022-09-07 13:35:47 +0300

    Premake setup.

---
+++ premake5.lua
@@ -0,0 +1,28 @@
+workspace "VolumeInspector"
+    architecture "x64"
+    configurations {"Debug", "Release"}
+
+outputDir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "VolumeInspector"
+    kind "ConsoleApp"
+    language "C++"
+    staticruntime "on"
+
+    targetdir ("bin" .. outputDir .. "/%{prj.name}")
+    objdir ("bin-int" .. outputDir .. "/%{prj.name}")
+
+    libdirs {"%{prj.location}External\Lib"}
+
+    filter "system.windows"
+        cppdialect "C++17"
+        systemversion "latest"
+
+    filter "configurations.Debug"
+        defines "DEBUG"
+        symbols "on"
+
+    filter "configurations.Release"
+        kind "WindowedApp"
+        defines "NDEBUG"
+        optimize "on"
